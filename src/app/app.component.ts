cbbb4e10dd2d46277d1a8ad35f1243ae79a8787b
adilmajd <62673904+adilmajd@users.noreply.github.com>
2025-09-04 12:41:21 +0100

    add component principales & sevices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContenuComponent } from "./contenu/contenu.component";
+import { HeadComponent } from './head/head.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContenuComponent, HeadComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
