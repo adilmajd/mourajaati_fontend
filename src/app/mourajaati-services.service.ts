fa9064dfb59f282cf8b58c4e7663747d3f6d026b
adilmajd <62673904+adilmajd@users.noreply.github.com>
2025-09-04 12:58:35 +0100

    add services

--- src/app/mourajaati-services.service.ts
+++ src/app/mourajaati-services.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable,inject } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+
+const baseUrl = 'http://127.0.0.1:8000/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MourajaatiServicesService {
-
+  private http = inject(HttpClient); // sert à récupérer un service (comme HttpClient, Router, etc.) sans passer par le constructor
   constructor() { }
+
+  get_all_users(){
+    return this.http.get(baseUrl + 'users/users/')
+  }
 }
