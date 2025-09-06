6bd21e99b049bdaca53b68f02b9bc5d075c59c0b
adilmajd <62673904+adilmajd@users.noreply.github.com>
2025-09-06 13:36:49 +0100

    add login component template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { ContenuComponent } from "./contenu/contenu.component";
 import { HeadComponent } from './head/head.component';
 import { FooterComponent } from "./footer/footer.component";
+import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContenuComponent, HeadComponent, FooterComponent],
+  imports: [RouterOutlet, ContenuComponent, HeadComponent, FooterComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
