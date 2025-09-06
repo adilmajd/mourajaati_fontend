2a63e5a390a3f5e73ed24a91a54c9a749f357055
adilmajd <62673904+adilmajd@users.noreply.github.com>
2025-09-06 12:29:38 +0100

    test get all users

--- src/app/contenu/contenu.component.ts
+++ src/app/contenu/contenu.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,inject,OnInit } from '@angular/core';
+import { MourajaatiServicesService } from '../mourajaati-services.service';
 
 @Component({
   selector: 'app-contenu',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './contenu.component.html',
   styleUrl: './contenu.component.css'
 })
-export class ContenuComponent {
+export class ContenuComponent implements OnInit {
+
+  private back_serv = inject(MourajaatiServicesService);
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  get_all_users_c(){
+    this.back_serv.get_all_users().subscribe(
+      (data:any)=>{
+        console.log(data)
+      }
+    );
+  }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent, {
+  ...appConfig,   // 👈 on étend la config existante
+  providers: [
+    ...(appConfig.providers || []), // 👈 on garde les anciens providers
+    provideHttpClient()             // 👈 on ajoute HttpClient
+  ]
+})
+.catch((err) => console.error(err));
