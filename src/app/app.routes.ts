df924180579d662ba3c7993f417bb15f16c8560c
adilmajd <62673904+adilmajd@users.noreply.github.com>
2025-09-11 12:48:38 +0100

    unauthorized & component aceuil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { authGuard } from './auth.guard';
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 export const routes: Routes = [
-    {path:"dashbord",component:LoginComponent,canActivate:[authGuard]},
-    {path:"login",component:LoginComponent},
+    {
+        path:"dashbord",
+        component:LoginComponent,
+        canActivate:[authGuard]},
+    {
+        path:"login",
+        component:LoginComponent
+    },{
+        path:"unauthorized",
+        component:UnauthorizedComponent
+    },
+    {
+        path:"",
+        component:AccueilComponent
+    }
 
 ];
